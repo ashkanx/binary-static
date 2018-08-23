7bb2631398f0a5c7271c7414812011bb3fe27a5c
ashkan <ashkan.nemati@gmail.com>
2018-08-23 15:50:46 +0800

    Deploy to gh-pages

--- js/binary_app.js
+++ js/binary_app.js
@@ -20876,8 +20876,10 @@ var SubscriptionManager = function () {
         // callback subscribers
         var subscribers = sub_info.subscribers;
         if (subscribers.length) {
-            if (!sub_info.stream_id) {
-                // first response returned error or not a subscription (i.e. subscribed proposal_open_contract for an expired contract)
+            // remove subscription info when first response returned error
+            // or not a subscription (i.e. subscribed proposal_open_contract for an expired contract)
+            // check msg_type to filter out those calls which don't return stream `id` on first response (tick_history, ...)
+            if (!sub_info.stream_id && (response.error || response.msg_type === sub_info.msg_type)) {
                 delete subscriptions[sub_id];
             }
             sub_info.subscribers.forEach(function (fnc) {
